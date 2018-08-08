26a3133ae3106fadb0803ac9dc32b5d666536d37
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2018-08-08 23:40:56 +0300

    Fine-tuned gcd.py

--- gcd.py
+++ gcd.py
@@ -12,10 +12,12 @@ class GoogleCloudDatastoreExporter(Exporter):
         return "Google Cloud Datastore"
     
     def export(self, measurements, ts=None):
+        if ts is None:
+            ts = datetime.datetime.utcnow()
         entities = []
         for mac, content in measurements:
             key = self._client.key("Measurement")
-            e = datastore.Entity(key=key, exclude_from_indexes=["temperature", "humidity", "pressure"])
+            e = datastore.Entity(key=key, exclude_from_indexes=("temperature", "humidity", "pressure"))
             e["mac"] = mac
             e["name"] = content["name"]
             e["ts"] = ts
