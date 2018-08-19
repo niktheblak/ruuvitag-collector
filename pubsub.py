045e544c2eb1f2597afcf926401a2a7f0b31f2ea
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2018-08-19 23:52:53 +0300

    Do not replace timezone information in pubsub.py

--- pubsub.py
+++ pubsub.py
@@ -23,7 +23,7 @@ class GooglePubSubExporter(Exporter):
             m = {}
             m["mac"] = mac
             m["name"] = content["name"]
-            m["ts"] = ts.replace(tzinfo=datetime.timezone.utc).isoformat()
+            m["ts"] = ts.isoformat()
             m["temperature"] = float(content["temperature"])
             m["humidity"] = float(content["humidity"])
             m["pressure"] = float(content["pressure"])
