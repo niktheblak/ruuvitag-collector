29ba2a3130a43af196e9f2b07d6a972e704eafa4
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2018-08-28 15:22:19 +0300

    Made InfluxDB measurement name configurable

--- influx.py
+++ influx.py
@@ -5,8 +5,10 @@ from exporter import Exporter
 from influxdb import InfluxDBClient
 
 class InfluxDBExporter(Exporter):
-    def __init__(self):
-        cfg = InfluxDBConfig()
+    def __init__(self, cfg=None):
+        if cfg is None:
+            cfg = InfluxDBConfig()
+        self._cfg = cfg
         self._client = InfluxDBClient(
             host=cfg.host,
             port=cfg.port,
@@ -25,11 +27,28 @@ class InfluxDBExporter(Exporter):
         if ts is None:
             ts = datetime.datetime.utcnow()
         for mac, content in measurements:
-            points = to_influx_points(ts, mac, content)
+            points = self._to_influx_points(ts, mac, content)
             self._client.write_points(points)
     
     def close(self):
         self._client.close()
+    
+    def _to_influx_points(self, ts, mac, content):
+        return [
+            {
+                "measurement": self._cfg.measurement,
+                "tags": {
+                    "name": content["name"],
+                    "mac": mac
+                },
+                "time": ts.isoformat(),
+                "fields": {
+                    "temperature": float(content["temperature"]),
+                    "humidity": float(content["humidity"]),
+                    "pressure": float(content["pressure"])
+                }
+            }
+        ]
 
 
 class InfluxDBConfig:
@@ -38,23 +57,7 @@ class InfluxDBConfig:
         self.host = os.environ.get("RUUVITAG_INFLUXDB_HOST", "localhost")
         self.port = int(os.environ.get("RUUVITAG_INFLUXDB_PORT", "8086"))
         self.database = os.environ.get("RUUVITAG_INFLUXDB_DATABASE")
+        self.measurement = os.environ.get("RUUVITAG_INFLUXDB_MEASUREMENT", "ruuvitag_sensor")
         self.username = os.environ.get("RUUVITAG_INFLUXDB_USERNAME", "root")
         self.password = os.environ.get("RUUVITAG_INFLUXDB_PASSWORD", "root")
         self.path = os.environ.get("RUUVITAG_INFLUXDB_PATH", "")
-
-def to_influx_points(ts, mac, content):
-    return [
-        {
-            "measurement": "ruuvitag_sensor",
-            "tags": {
-                "name": content["name"],
-                "mac": mac
-            },
-            "time": ts.isoformat(),
-            "fields": {
-                "temperature": float(content["temperature"]),
-                "humidity": float(content["humidity"]),
-                "pressure": float(content["pressure"])
-            }
-        }
-    ]
